94abd2f8c434f28dc62ba47c5f87df2f209e4fd6
Miguel Couto <miguelitoboas@gmail.com>
2013-12-28 21:21:22 +0000

    Version 1: Added a basic Menu to Place Plants. Added basic Collisions.

--- examples/miguel/javascripts/miguel-scenes.js
+++ examples/miguel/javascripts/miguel-scenes.js
@@ -4,6 +4,35 @@
       var bg = stage.insert(new Q.Background());
       var shoot = stage.insert(new Q.Shooter());
       var zoombie = stage.insert(new Q.Walker({ }));
+
+      window.listPlaces = [];
+      function placeMe(){
+        stage.insert(new Q.Shooter({
+          x: this.p.x,
+          y: this.p.y,
+        }));
+        Q._each(listPlaces, function(a){
+          a.destroy();
+        });
+      }
+      stage.insert(new Q.UI.Button({
+        asset: 'actors/Shoot.png',
+        y: 150,
+        x: 100,
+        border: 2,
+      }, function() {
+        //what to do here^^
+        for (var y = 0; y <= 5; y++) {
+          for (var x = 0; x <= 8; x++) {
+            var pl = new Q.Place({
+              x : x * 84 + 280,
+              y : y * 100 + 150
+            }, placeMe);
+            listPlaces.push(pl);
+            stage.insert(pl);
+          }
+        }
+      }));
   });
 };
 
--- examples/miguel/javascripts/miguel-ui.js
+++ examples/miguel/javascripts/miguel-ui.js
@@ -22,6 +22,7 @@
   Q.Sprite.extend("Plant",{
     init: function(p1, p2){
       this._super(Q._extend(p2, p1),{
+        type: Q.SPRITE_FRIENDLY
       });
       this.on('step', this, this.step);
     },
@@ -29,12 +30,20 @@
     Shooting: {
       last: 0,
       count: 0,
-      max: 100
+      max: 400
     },
     Shoot: function(st){
       //override this
     },
+    OnHit: function(st, enemy){
+      //override this
+      console.log(enemy.className);
+    },
     step: function(st){
+      var collided = this.stage.search(this, Q.SPRITE_ENEMY);
+      if (collided){
+        this.OnHit(st, collided.obj);
+      }
       if (this.Shooting.count > this.Shooting.max){
         //Shoot
         this.Shoot(st);
@@ -48,6 +57,8 @@
     init: function(p1, p2){
       this._super(Q._extend(p2, p1),{
         vx: 70,
+        type: Q.SPRITE_NONE,
+        collisionMask: Q.SPRITE_ENEMY
       });
       this.add("2d");
     },
@@ -58,10 +69,16 @@
     init: function(p1, p2){
       this._super(Q._extend(p2, p1),{
         vx: -10,
+        type: Q.SPRITE_ENEMY,
+        collisionMask: Q.SPRITE_FRIENDLY
       });
       this.add("2d");
+      this.on("hit.sprite", function(collision){
+        this.p.vx = this.Speed;
+      })
     },
-    health: 10
+    Speed: -10,
+    health: 100
   },{});
 
   Q.Plant.extend("Shooter",{
@@ -69,14 +86,17 @@
       this._super(p,{
         x: 280,
         y: 150,
-        asset: 'actors/Shoot.png',
-        type: 0
+        asset: 'actors/Shoot.png'
       });
     },
     Shoot: function(st){
       //Need to Shoot now
       console.log("Shoot....");
-      this.stage.insert(new Q.Ball({ x: this.p.x, y: this.p.y }));
+      this.stage.insert(new Q.Ball({ 
+        x: this.p.x + 20, 
+        y: this.p.y - 15,
+        z: 0
+      }));
     }
   });
 
@@ -85,10 +105,25 @@
       this._super(p,{
         x: Q.width -600,
         y: 150,
-        asset: 'actors/Zombie.png',
-        type: 0
+        asset: 'actors/Zombie.png'
       });
-    }
+      /*
+      this.on("hit.sprite", function(collision){
+        if(collision.obj instanceof Q.Plant) {
+          console.log("Hit Plant...");
+          this.Speed = -1;
+          this.p.vx = this.Speed;
+          collision.obj.health -= this.Hit;
+          if (collision.obj.health < 0){
+            collision.obj.destroy();
+            this.Speed = -10;
+            this.p.vx = this.Speed;
+          }
+        }
+      });
+      */
+    },
+    Hit: 1
   });
 
   Q.Weapon.extend("Ball",{
@@ -97,17 +132,31 @@
         asset: 'actors/Ball.png'
       });
       this.on("hit.sprite",function(collision) {
-      // Check the collision, if it's the Tower, you win!
-      if(collision.obj.isA("Zombie")) {
-        // Remove the player to prevent them from moving
-        this.destroy();
+        // Check the collision, if it's the Tower, you win!
+        if(collision.obj instanceof Q.Zombie) {
+          console.log("Hit Zombie....");
+          // Remove the player to prevent them from moving
+          this.destroy();
 
-        collision.obj.health -= this.health;
-        if (collision.obj.health < 0){
-          collision.obj.destroy();
+          collision.obj.health -= this.health;
+          if (collision.obj.health < 0){
+            collision.obj.destroy();
+          }
         }
-      }
-    });
+      });
     }
+  });
+
+
+  Q.UI.Button.extend("Place", {
+    init: function (p,callback) {
+      this._super(Q._extend({
+        label: 'Here',
+        w: 100,
+        h: 100,
+        type : Q.SPRITE_UI
+      }, p), callback);
+    },
+    border: 2,
   });
 };
